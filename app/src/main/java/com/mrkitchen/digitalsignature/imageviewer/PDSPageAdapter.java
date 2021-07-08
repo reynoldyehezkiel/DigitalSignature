60b4af676d36e4c495d7d92bf54e73c26cb722fd
reynoldyehezkiel <73789263+reynoldyehezkiel@users.noreply.github.com>
2021-07-08 09:35:49 +0700

    Change application ID, package & domain name and many more
    Change:
    - Color style
    - Some text
    - App icon

---
+++ app/src/main/java/com/mrkitchen/digitalsignature/imageviewer/PDSPageAdapter.java
@@ -0,0 +1,26 @@
+package com.mrkitchen.digitalsignature.imageviewer;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import com.mrkitchen.digitalsignature.Document.PDSFragment;
+import com.mrkitchen.digitalsignature.PDF.PDSPDFDocument;
+
+public class PDSPageAdapter extends FragmentStatePagerAdapter {
+
+    private PDSPDFDocument mDocument;
+
+    public PDSPageAdapter(FragmentManager fragmentManager, PDSPDFDocument fASDocument) {
+        super(fragmentManager);
+        this.mDocument = fASDocument;
+    }
+
+    public int getCount() {
+        return this.mDocument.getNumPages();
+    }
+
+    public Fragment getItem(int i) {
+        return PDSFragment.newInstance(i);
+    }
+
+}
